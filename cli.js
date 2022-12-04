1f86483234244ed9b4653ecafca5674a3072d8e6
ankishenoyy <ankitha@unc.edu>
2022-12-04 14:11:03 -0500

    cleaned up descriptions and weather suggestion output

--- cli.js
+++ cli.js
@@ -7,14 +7,16 @@ import fetch from "node-fetch";
 const args = minimist(process.argv.slice(2))
 let timezone = moment.tz.guess()
 
+// help description
 if (args.h) {
-    console.log("Usage: whatToWear.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE")
+    console.log("How does this work? Call node whatToWear.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE")
     console.log("    -h            Show this help message and exit.")
     console.log("    -n, -s        Latitude: N positive; S negative.")
     console.log("    -e, -w        Longitude: E positive; W negative.")
     console.log("    -z            Time zone: uses tz.guess() from moment-timezone by default.")
     process.exit(0);
 }
+
 // Determine timezone
 if (args.z) {
     timezone = args.z
@@ -40,37 +42,54 @@ if (args.e) {
     console.log("Longitude is out of range")
 }
 // Connect to Weather API
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m&daily=precipitation_hours&current_weather=true&timezone=' + timezone);
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m&daily=apparent_temperature_min&daily=apparent_temperature_max&daily=precipitation_hours&daily=windspeed_10m_max&daily=shortwave_radiation_sum&current_weather=true&timezone=' + timezone);
 const data = await response.json()
-const day = 0
+const day = 1
 
-console.log("For today's weather conditions you will need:")
+// Print today's weather
+console.log("\nThe weather today is...")
+var farHigh= data.daily.apparent_temperature_max[day]*(9/5)+32;
+var farLow= data.daily.apparent_temperature_min[day]*(9/5)+32;
+console.log("A high of " +farHigh + "°F and A low of " + farLow +"°F");
+console.log("It will rain for "+ data.daily.precipitation_hours[day]+ " hours today." );
+var windSpdmph= Math.round(data.daily.windspeed_10m_max[day]/1.609);
+console.log("The windspeed will be aboout " +windSpdmph+" mph" );
+var uvradiation= Math.round(data.daily.shortwave_radiation_sum[day]/10);
+console.log("The UV index is " +uvradiation );
+
+//What to wear suggestions
+
+//rain conditions
+console.log("\nFor today's weather conditions you will need...")
 if (data.daily.precipitation_hours[day] > 0) {
     console.log("Today will be raining, so a rain jacket or umbrella would be great!")
 }
-if(data.daily.apparent_temperature_min<8){
+//temperature conditions
+if(farLow<45){
     console.log("Brrr, it is cold out, the lows are below 45 degrees Farenheit, so you should pack a jacket")
 }
-
-else if(data.daily.apparent_temperature_min<16){
+else if(farLow<60){
     console.log("Today will be slightly chilly, the lows are around 60 degrees Farenheit, so you should pack a sweatshirt")
 }
-
-else if(data.daily.apparent_temperature_min<23){
+else if(farLow<73){
     console.log("The weather looks nice today, you could wear a shirt and pants")
 }
-
-else if(data.daily.apparent_temperature_min>=23){
+else if(farLow>=73){
     console.log("WOW, it's so hot out today! A tank top and shorts may be ideal for this weather")
 }
-if(data.daily.windspeed_10m_max>10){
+
+//windspeed conditions
+if(data.daily.windspeed_10m_max[day]>10){
     console.log("Today is going to be windy, make sure to pack a windbreaker")
 }
-
-if(data.daily.shortwave_radiation_sum>4000000){
-    console.log("The UV index is quite high today (>4) make sure to pack a hat, sunscreen, and sunfglasses!")
+else if(data.daily.windspeed_10m_max[day]<=10){
+    console.log("At least the wind doesn't look too bad today")
 }
 
-else if(data.daily.shortwave_radiation_sum>4000000){
-    console.log("It's not too sunny today, but everyday is a great day to wear sunscreen--  better than sorry!")
+//radiation conditions
+if(uvradiation>4){
+    console.log("The UV index is quite high today!! make sure to pack a hat, sunscreen, and sunglasses!")
+}
+else if(uvradiation<4){
+    console.log("It's not too sunny today, but everyday is a great day to wear sunscreen- better safe than sorry!")
 }
